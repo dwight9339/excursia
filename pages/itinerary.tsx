0bfa939ec8379ca9cdd1a521a498ba137c352b3e
David White <whited9339@gmail.com>
2023-03-22 12:26:15 -0600

    Added beautiful-dnd library

--- pages/itinerary.tsx
+++ pages/itinerary.tsx
@@ -1,20 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Typography, Box, ListItem, ListItemText } from '@mui/material';
 import { useRouter } from 'next/router';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-const ItineraryPage: React.FC = () => {
-  const router = useRouter();
-
-  // TODO: Fetch the generated itinerary from the server and display it
-
-  return (
-    <div>
-      <h1>Your Itinerary</h1>
-      {/* TODO: Display the itinerary information at the top of the page */}
-      {/* TODO: Display the itinerary as a list of activities */}
-      {/* TODO: Allow users to reorder and delete activities */}
-      <button onClick={() => router.back()}>Go Back</button>
-    </div>
-  );
-};
-
-export default ItineraryPage;
