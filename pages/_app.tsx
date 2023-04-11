9897d3cb1bb0b705767abe0339206e01727551b1
David White <whited9339@gmail.com>
2023-04-11 12:11:24 -0600

    Updated _app.js to tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { SessionProvider } from 'next-auth/react';
 import Header from "../components/Header";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
