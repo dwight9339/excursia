a834d1532734b53723a3f753c10d830804a4896b
David White <whited9339@gmail.com>
2023-05-03 12:40:41 -0600

    Installed and set up sass

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ["maps.googleapis.com", "maps.gstatic.com"]
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
   }
 }
 
