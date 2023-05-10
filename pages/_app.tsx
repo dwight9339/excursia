145fc8a7989151378d95d8f3d5246ab879fbd316
David White <whited9339@gmail.com>
2023-05-10 11:41:44 -0600

    Updated global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
-import '../styles/globals.css';
+import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Head>
+          <title>Excursia</title>
+          <meta name="description" content="Excursia" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <Header />
         <Component {...pageProps} />
       </LocalizationProvider>
