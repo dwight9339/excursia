ea053228cbf66b541219033dffab5a394ebc72a0
David White <whited9339@gmail.com>
2023-05-30 05:59:15 -0600

    Updated styles on unauthorized user component

--- components/UnauthorizedUser.tsx
+++ components/UnauthorizedUser.tsx
@@ -11,20 +11,41 @@ const UnauthorizedUser: React.FC = () => {
   };
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '80vh',
+      }}
     >
-      <Typography variant="h4" align="center" gutterBottom>
-        You do not have permission to edit this itinerary.
-      </Typography>
-      <div className={commonStyles.buttonPrimary} onClick={handleRedirect}>
-        Home
+      <div 
+        className={commonStyles.box}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '50vw',
+          padding: '4rem',
+        }}
+      >
+        <div
+          style={{
+            fontSize: '2rem',
+            fontWeight: 'bold',
+            marginBottom: '3rem',
+            textAlign: 'center',
+          }}
+        >
+          You do not have permission to edit this itinerary.
+        </div>
+        <div className={commonStyles.buttonPrimary} onClick={handleRedirect}>
+          Home
+        </div>
       </div>
-    </Box>
+    </div>
   );
 };
 
