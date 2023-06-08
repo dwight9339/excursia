dbc025af7303f17611d87b230f9f18171620ebda
David White <whited9339@gmail.com>
2023-06-08 09:25:06 -0600

    Updated styles for my itiineraries page including adding customized loading spinner element, no itineraries box with button to create new itinerary, and changes to the itinerary list and list item classes

--- pages/my-itineraries/index.tsx
+++ pages/my-itineraries/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
+import { CircularProgress } from "@mui/material";
 import styles from "../../styles/MyItineraries.module.scss";
 
 const ItineraryListItem: React.FC<{ itinerary: Itinerary }> = ({ itinerary }) => {
@@ -20,6 +21,7 @@ const ItineraryListItem: React.FC<{ itinerary: Itinerary }> = ({ itinerary }) =>
 };
 
 const UserItineraries: React.FC = () => {
+  const router = useRouter();
   const { data: session, status } = useSession();
   const userId = session?.user?.id;
   const [itineraries, setItineraries] = useState<Itinerary[]>([]);
@@ -41,36 +43,57 @@ const UserItineraries: React.FC = () => {
     fetchItineraries();
   }, [userId]);
 
-  if (loading) {
-    return <div>Loading...</div>;
+  if (loading || status === "loading") {
+    return (
+      <div className={styles.container}>
+        <div className={styles.loadingContainer}>
+          <CircularProgress
+            className={styles.spinner}
+            color="inherit"
+          />
+        </div>
+      </div>
+    );
   }
 
   if (itineraries.length === 0) {
-    return <div>No itineraries found.</div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.noItinerariesContainer}>
+          <div className={styles.noItinerariesText}>
+            You have no itineraries yet.
+          </div>
+          <div 
+            className={styles.newItineraryButton}
+            onClick={() => router.push("/")}
+          >
+            Create New Itinerary
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
     <div className={styles.container}>
+      <div className={styles.pageHead}>
+        <h1>My Itineraries</h1>
+        <div className={styles.sortByContainer}>
+          <label htmlFor="sortBy">Sort by:</label>
+          <select 
+            name="sortBy"
+            id="sortBy"
+            className={styles.sortBy}
+          >
+            <option value="date">Date</option>
+            <option value="name">Name</option>
+          </select>
+        </div>
+      </div>
       <div className={styles.listContainer}>
-        <div className={styles.pageHead}>
-          <h1>My Itineraries</h1>
-          <div className={styles.sortByContainer}>
-            <label htmlFor="sortBy">Sort by:</label>
-            <select 
-              name="sortBy"
-              id="sortBy"
-              className={styles.sortBy}
-            >
-              <option value="date">Date</option>
-              <option value="name">Name</option>
-            </select>
-          </div>
-        </div>
-        <div className={styles.itineraryList}>
-          {itineraries.map((itinerary) => (
-            <ItineraryListItem itinerary={itinerary} key={itinerary.id} />
-          ))}
-        </div>
+        {itineraries.map((itinerary) => (
+          <ItineraryListItem itinerary={itinerary} key={itinerary.id} />
+        ))}
       </div>
     </div>
   );
