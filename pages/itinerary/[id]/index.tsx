7a89152f5302e46bcced73f5cea3e7c70a3fcd04
David White <whited9339@gmail.com>
2023-07-03 13:03:34 -0600

    Load itinerary on client side to reduce bundle size

--- pages/itinerary/[id]/index.tsx
+++ pages/itinerary/[id]/index.tsx
@@ -1,43 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
+import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
-import { ParsedUrlQuery } from 'querystring';
-import { fetchItinerary } from '../../../lib/dbFetch';
+import { CircularProgress } from '@mui/material';
 import ItineraryInfoPage from '../../../components/ItineraryInfoPage';
-import styles from "../../../styles/ItineraryPage.module.scss";
+import axios from 'axios';
 
-interface ItineraryPageProps {
-  itinerary: Itinerary;
-}
-
-const ItineraryPage: React.FC<ItineraryPageProps> = ({ itinerary }) => {
+const ItineraryPage: React.FC = () => {
   const { data, status } = useSession();
   const userData: any = { ...data?.user };
+  const router = useRouter();
+  const { id } = router.query;
+  const [itinerary, setItinerary] = useState<Itinerary | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  if (!itinerary) return <div>Itinerary not found</div>;
+  useEffect(() => {
+    const fetchItineraryData = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(`/api/fetch-itinerary?id=${id}`);
+
+        if (res.status !== 200) {
+          console.error(`Error fetching itinerary: ${res.statusText}`);
+          setItinerary(null);
+        } else {
+          setItinerary(res.data);
+        }
+      } catch (err) {
+        console.error(`Error fetching itinerary: ${err}`);
+        setItinerary(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (id) {
+      fetchItineraryData();
+    }
+  }, [id]);
+
+  const pageContent = useMemo(() => {
+    if (loading) {
+      return (
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          width: '100vw',
+        }}>
+          <CircularProgress
+            size={300}
+            style={{
+              color: "#0A89A6"
+            }}
+          />
+        </div>
+      );
+    }
+
+    if (!loading && !itinerary) {
+      return <div>Itinerary not found</div>;
+    }
+
+    if (itinerary) {
+      return <ItineraryInfoPage itinerary={itinerary} />;
+    }
+
+    return <div>Something went wrong...</div>;
+  }, [loading, itinerary]);
 
   return (
-    <ItineraryInfoPage itinerary={itinerary} />
+    <div>
+      {pageContent}
+    </div>
   );
-};
-
-export async function getServerSideProps(context: ParsedUrlQuery) {
-  try {
-    const params: any = context.params;
-    const { id } = params;
-    console.log(`Fetching itinerary with id ${id}`);
-    const res = await fetchItinerary(id);
-
-    if (res) {
-      console.log("Itinerary retrieved");
-      return { props: { itinerary: res }};
-    } else {
-      console.log(`Unable to retrieve itinerary with id ${id}`);
-      return { props: { itinerary: {}}}; 
-    }
-  } catch(err) {
-    console.log(`Error: ${err}`);
-    return { props: { itinerary: {}}};
-  }
 };
 
 export default ItineraryPage;
