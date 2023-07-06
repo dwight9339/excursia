7f838fd999619a211877600a325c02f9bd3d88a3
David White <whited9339@gmail.com>
2023-07-06 10:00:22 -0600

    Disabled SSR for entire app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { SessionProvider } from 'next-auth/react';
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import Header from "../components/Header";
 import ModalProvider from '../providers/ModalProvider';
@@ -27,4 +28,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+export default dynamic(() => Promise.resolve(MyApp), {
+  ssr: false,
+});
