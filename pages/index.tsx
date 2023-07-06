d9d92244f0bf01ba83789f8b589d5e6ae0da6abd
David White <whited9339@gmail.com>
2023-07-06 05:52:01 -0600

    Use getSession hook to check session status on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
-import React, { useMemo } from 'react';
-import { useSession } from 'next-auth/react';
+import React, { useMemo, useEffect, useState } from 'react';
+import { getSession } from 'next-auth/react';
 import { CircularProgress } from '@mui/material';
 import WelcomeBanner from '../components/WelcomeBanner';
 import styles from "../styles/Home.module.scss";
 import NewItinerary from '../components/ActivitySearch/NewItinerary';
 
 const HomePage: React.FC = () => {
-  const { status } = useSession();
+  const [status, setStatus] = useState<"loading" | "unauthenticated" | "authenticated">("loading");
 
-  
+  useEffect(() => {
+    const updateSession = async () => {
+      const _session = await getSession();
+      if (_session) {
+        setStatus("authenticated");
+      } else {
+        setStatus("unauthenticated");
+      }
+    };
+
+    if (status === "loading") updateSession();
+  }, []);
 
   const pageContent = useMemo(() => {
     if (status === "loading") {
