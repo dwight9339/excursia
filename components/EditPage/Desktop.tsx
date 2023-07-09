da3b7fcd979956f655e7a04e088bebb20a289a05
David White <whited9339@gmail.com>
2023-07-09 14:12:07 -0600

    Moved AddActivity component to EditPage folder

--- components/EditPage/Desktop.tsx
+++ components/EditPage/Desktop.tsx
@@ -3,7 +3,7 @@ import styles from "../../styles/EditItinerary.module.scss";
 import ItineraryMap from '../ItineraryMap';
 import ActivityList from './ActivityList';
 import SuggestedActivities from './SuggestedActivities';
-import AddActivity from '../AddActivity';
+import AddActivity from './AddActivity';
 import EditableText from '../EditableText';
 import ModalContext from '../../contexts/ModalContext';
 import UpdateSearch from '../ActivitySearch/UpdateSearch';
