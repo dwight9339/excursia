4e69772bc4a4315474da76b27bef091e62399bf7
David White <whited9339@gmail.com>
2023-07-09 14:10:53 -0600

    Moved ActivityList component to EditPage folder

--- components/EditPage/Desktop.tsx
+++ components/EditPage/Desktop.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import styles from "../../styles/EditItinerary.module.scss";
 import ItineraryMap from '../ItineraryMap';
-import ActivityList from '../ActivityList';
+import ActivityList from './ActivityList';
 import SuggestedActivities from './SuggestedActivities';
 import AddActivity from '../AddActivity';
 import EditableText from '../EditableText';
