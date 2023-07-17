0dd472791ac20541ef86f4765e08755d306735dd
David White <whited9339@gmail.com>
2023-07-17 12:44:15 -0600

    Added new unit tests to test location input functionality

--- tests/ActivitySearchForm.test.js
+++ tests/ActivitySearchForm.test.js
@@ -1,7 +1,36 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import ActivitySearchForm from '../components/ActivitySearch/ActivitySearchForm';
 import { SessionProvider } from 'next-auth/react';
+import { useLoadScript } from '@react-google-maps/api';
+import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete';
+
+// Mock the useLoadScript hook
+jest.mock('@react-google-maps/api', () => ({
+  useLoadScript: jest.fn(),
+  GoogleMap: ({ children }) => <div>{children}</div>,
+  CircleF: () => <div />
+}));
+
+// Mock the usePlacesAutocomplete hook
+jest.mock('use-places-autocomplete', () => ({
+  __esModule: true,
+  default: jest.fn(),
+  getGeocode: jest.fn(),
+  getLatLng: jest.fn()
+}));
+
+// Create a mock google object
+global.google = {
+  maps: {
+    LatLng: function(lat, lng) {
+      return { lat: lat, lng: lng };
+    },
+    MapTypeId: {
+      ROADMAP: 'roadmap',
+    },
+  },
+};
 
 describe('ActivitySearchForm', () => {
   const mockUpdateItinerary = jest.fn();
@@ -29,6 +58,57 @@ describe('ActivitySearchForm', () => {
       },
     }
   };
+  let mockLocationInputValue = "";
+
+
+  beforeEach(() => {
+    mockLocationInputValue = "";
+
+    // Setup the useLoadScript hook to return isLoaded: true
+    useLoadScript.mockReturnValue({
+      isLoaded: true,
+      loadError: null,
+    });
+
+    // Setup the usePlacesAutocomplete hook to return some mock data
+    usePlacesAutocomplete.mockReturnValue({
+      ready: true,
+      value: mockLocationInputValue,
+      suggestions: { status: 'OK', data: [
+        {
+          name: 'Test Suggestion 1',
+          geometry: {
+            location: {
+              lat: 50.79,
+              lng: -85.07,
+            },
+          },
+        },
+        {
+          name: 'Test Suggestion 2',
+          geometry: {
+            location: {
+              lat: 40.7128,
+              lng: -74.0060,
+            },
+          },
+        },
+      ] },
+      setValue: jest.fn(),
+      clearSuggestions: jest.fn()
+    });
+
+    getGeocode.mockImplementation(() => Promise.resolve([{
+      geometry: {
+        location: {
+          lat: () => 50.79,
+          lng: () => -85.07,
+        },
+      },
+    }]));
+  
+    getLatLng.mockImplementation(() => Promise.resolve({ lat: 50.79, lng: -85.07 }));
+  });
 
   it("renders all sections without crashing", () => {
     const { getByTestId } = render(
@@ -45,6 +125,18 @@ describe('ActivitySearchForm', () => {
     expect(getByTestId("activity-search-form--search-radius-slider-container")).toBeInTheDocument();
     expect(getByTestId("activity-search-form--map-container")).toBeInTheDocument();
     expect(getByTestId("activity-search-form--interest-select-container")).toBeInTheDocument();
+  });
+
+  it("renders location search bar", () => {
+    const { getByTestId } = render(
+      <SessionProvider
+        session={mockSession}
+      >
+        <ActivitySearchForm itinerary={defaultItinerary} updateItinerary={mockUpdateItinerary} />
+      </SessionProvider>
+    );
+
+    expect(getByTestId("location-search--input")).toBeInTheDocument();
   });
 
   it("updates search radius when search radius slider is moved", () => {
@@ -83,5 +175,27 @@ describe('ActivitySearchForm', () => {
 
     const slider = getByRole('slider');
     expect(slider).toBeDisabled();
+  });
+
+  it("enables search radius slider when location is selected", async () => {
+    const { getByText, getByTestId, findByTestId, getByRole } = render(
+      <SessionProvider
+        session={mockSession}
+      >
+        <ActivitySearchForm
+          itinerary={defaultItinerary}
+          updateItinerary={mockUpdateItinerary}
+          isDraft={true}
+        />
+      </SessionProvider>
+    );
+
+    const slider = getByRole('slider');
+    expect(slider).toBeDisabled();
+
+    const locationInput = getByTestId("location-search--input");
+    fireEvent.change(locationInput, { target: { value: "Test Location" } });
+    fireEvent.click(await findByTestId("location-search--suggestion-0"));
+    await waitFor(() => expect(slider).not.toBeDisabled());
   });
 });
